1eaf60faf79442724427f6b250c08645b3d7989f
Tanvir101cmd <tanbirulislamjj@gmail.com>
2026-09-23 19:38:12 +0600

    feat: read text from system clipboard

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import ()
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func clipboard_read() (string, error) {
+  var cmd *exec.Cmd
+
+ 	if runtime.GOOS == "darwin" {
+		cmd = exec.Command("pbpaste")
+	} else if runtime.GOOS == "linux" {
+		cmd = exec.Command("xclip", "-selection", "clipboard", "-o")
+	} else {
+		return "", fmt.Errorf("unsupported OS: %s", runtime.GOOS)
+	}
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return out.String(), nil
+}
 
 func main() {
-
+  text, err := clipboard_read()
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println("clipboard:", text)
 }
